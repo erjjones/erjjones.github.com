c082cabb84cd923093f4d141cd5defe8f35f67d9
Eric Jones <ejones@et.local>
2012-03-05 23:51:25 -0500

    github stat buttons

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function() {
+	$.ajax({
+			 type: "GET",
+			 url: "https://api.github.com/users/erjjones",
+			 async: true,
+		 dataType: "json",
+		 success: function(data){							
+			$('#gf').text(data.followers + ' GitHub Followers');
+			$('#gfr').text(data.public_repos + ' GitHub Repos');
+		 }
+		});
+		
+	$('#ghw').githubWidget({
+			'username': 'Erjjones',
+			'displayActions': false,
+			'firstCount': 10,
+			'displayHeader': false,
+			'displayLastCommit': false,
+			'displayAccountInformations': false,
+			'displayLanguage': false
+		});
+});
