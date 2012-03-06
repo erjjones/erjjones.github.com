5dbce33bdfcbf1ce6b5d0d1a9ef9c6f6f957c572
Eric Jones <ejones@et.local>
2012-03-06 17:16:53 -0500

    jsonp try no. 2

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,26 @@ jQuery(document).ready(function() {
 	$('#gf').text('6 GitHub Followers');
     $('#gfr').text('5 GitHub Repos');		
 	
-	jQuery.getJSON("https://api.github.com/users/erjjones&callback=?", function(data) {
+	JSONP( 'https://api.github.com/users/erjjones?callback=?', function( response ) {
+		console.log(response);
+	});
+	
+	function JSONP( url, callback ) {
+		var id = ( 'jsonp' + Math.random() * new Date() ).replace('.', '');
+		var script = document.createElement('script');
+		script.src = url.replace( 'callback=?', 'callback=' + id );
+		document.body.appendChild( script );
+		window[ id ] = function( data ) {
+			if (callback) {
+				callback( data );
+			}
+		};
+	}
+	
+	/*jQuery.getJSON("https://api.github.com/users/erjjones&callback=?", function(data) {
 		$('#gf').text(data.followers + ' Followers');
         $('#gfr').text(data.public_repos + ' Repos');	
-	});
+	});*/
 	
 	/*$.get('https://api.github.com/users/erjjones', function(res) { 			
 		var obj = jQuery.parseJSON(res);	
