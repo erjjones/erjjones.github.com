3f71c6a9bcddd73793dc1990dc01d810014dd5bb
Eric Jones <ejones@et.local>
2012-03-06 09:49:36 -0500

    waiting

--- js/custom.js
+++ js/custom.js
@@ -11,14 +11,19 @@ jQuery(document).ready(function() {
 		});
 		*/
 	$.get('https://api.github.com/users/erjjones', function(res) { 		
+		console.log(res);
 		var obj = jQuery.parseJSON(res);	
 		if(typeof obj.followers != 'undefined')		
 		{
+			console.log('logging obj');
+			console.log(obj);
 			$('#gf').text(obj.followers + ' GitHub Followers');
 			$('#gfr').text(obj.public_repos + ' GitHub Repos');		
 		}
 		else
 		{
+			console.log('not logging obj');
+			console.log(obj);
 			$('#gf').text('10 GitHub Followers');
 		    $('#gfr').text('5 GitHub Repos');		
 		}		
